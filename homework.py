000fb993622851c61c75a09c27dae1d062f8d986
Derek <derek.yao.graham99@gmail.com>
2026-05-17 21:11:47 +1000

    Fix print statement and update name in list
    Fixed print statement to display 'Jake' and updated 'Tony' to 'Lily' in the names list.

--- homework.py
+++ homework.py
@@ -4,9 +4,10 @@
 names = ["Alex", "Jake", "Bryan", "Tony"]
 
 # Task 1: Print "Jake" from the list.
-print()  # TO DO
+print(name[1])  # TO DO
 
 # Task 2: In the same list, change "Tony" to "Lily". Then print the list
+name[3] = Lily
 print()  # TO DO
 
 
